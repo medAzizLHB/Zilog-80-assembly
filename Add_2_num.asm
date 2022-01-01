06fe6ecc77e16e8e6eece0da7c2daf6a6a43b181
Mohamed Abdelaziz Louhab <86206955+medAzizLHB@users.noreply.github.com>
2022-01-01 22:06:29 +0100

    two numbers addition

---
+++ Add_2_num.asm
@@ -0,0 +1,7 @@
+; medAzizLHB
+; Add two numbers
+
+LD A, 67H       ;  load register A with value 67H
+LD B, 12H       ;  load register B with value 12H
+ADD A, B        ;  add content of register B to register A and stores it in register A 
+HALT            ;  stop the program
